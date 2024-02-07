d7cbae8b374d8cb55e37ce7290ee122a39ac7798
Shmupo <andrewdoan8b@gmail.com>
2024-02-06 23:15:41 -0800

    Added home.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './Pages/Home'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello There.
+          <Home></Home>
         </p>
       </header>
     </div>
---
+++ src/Pages/Home.js
@@ -0,0 +1,23 @@
+//import './Home.css';
+
+//import Projects from '../components/Projects' 
+//import Skills from '../components/Skills'
+
+function Home() {
+    return (
+        <div>
+            <div className='intro'>
+                <div className='left-half'></div>
+                <div className='right-half'>
+                    <h1 className='name'>Andrew Doan</h1>
+                    <h2 className='role'>&lt;Developer&gt;</h2>
+                    <p className='description'></p>
+                </div>
+            </div>
+            <section>
+            </section>
+        </div>
+    );
+}
+
+export default Home;
