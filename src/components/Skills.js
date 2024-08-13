e90bee7c090b04ef96fb79b3ff88eb84a583f20e
Shmupo <andrewdoan8b@gmail.com>
2024-08-13 09:27:25 -0700

    Modification for public release

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -2,9 +2,7 @@ import './Skills.css'
 
 import { ReactComponent as CsSvg } from '../images/c#.svg';
 import { ReactComponent as CppIcon } from '../images/c++_icon.svg';
-import { ReactComponent as DjangoIcon } from '../images/django_icon.svg';
 import { ReactComponent as PythonIcon } from '../images/python_icon.svg';
-import { ReactComponent as ReactIcon } from '../images/react_icon.svg';
 
 import GodotIcon from '../images/Godot_icon.png';
 import HtmlIcon from '../images/html5_icon.png';
@@ -18,8 +16,6 @@ function Skills() {
                     <CsSvg title='C#' />
                     <CppIcon title='C++' />
                     <PythonIcon title='Python' class='python' />
-                    <DjangoIcon title='Django' />
-                    <ReactIcon title='React' />
                 <img src={GodotIcon} title='Godot' />
                 <img src={HtmlIcon} title='Html 5' />
                 <img src={CssIcon} title='CSS 3' />
